d8e6b2f63f5ab9c019eb2b428e2d4e87fa20f8e9
C4m1lly <dofuturoprojetos@gmail.com>
2025-02-17 15:18:34 -0300

    Criação das rotas Delete/Temas/Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import { AuthProvider } from './contexts/AuthContext'
 import Login from './pages/login/Login'
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import FormTema from './components/temas/formtema/FormTema'
+import DeletarTema from './components/temas/deletartema/DeletarTema'
 
 
 function App() {
@@ -21,6 +24,10 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
